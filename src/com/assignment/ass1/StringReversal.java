5af084768a75a96b0f54f01fe9a07ade205faaa4
viswesh <56759684+visweshamajala@users.noreply.github.com>
2020-07-29 19:12:23 +0530

    Add files via upload

---
+++ src/com/assignment/ass1/StringReversal.java
@@ -0,0 +1,15 @@
+package com.assignment.ass1;
+
+public class StringReversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Hello World";
+		
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
